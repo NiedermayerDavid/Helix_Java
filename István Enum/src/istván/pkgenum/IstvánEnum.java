0269876db3984932ffefd6685d7f234c74581bef
NiedermayerDavid <145555492+NiedermayerDavid@users.noreply.github.com>
2024-04-13 14:21:06 +0200

    oraimunka

---
+++ István Enum/src/istván/pkgenum/IstvánEnum.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package istván.pkgenum;
+
+/**
+ *
+ * @author nidid
+ */
+public class IstvánEnum {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ István Enum/src/istván/pkgenum/enumPhone.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package istván.pkgenum;
+
+/**
+ *
+ * @author nidid
+ */
+public class enumPhone {
+    
+    enum telefon {
+        iPhone,
+        Samsung,
+        Xiaomi,
+        Poco
+    }
+        
+    
+}
